01693400aa96a2ac3ad683718c82104b057c2453
Kirsten Strydom <2719827@students.wits.ac.za>
2026-03-02 16:39:27 +0200

    neighbours show... spinner doesn't

--- script.js
+++ script.js
@@ -8,26 +8,17 @@ async function searchCountry(countryName) {
     errorDiv.classList.add("hidden");
     countryInfo.innerHTML = "";
     bordersDiv.innerHTML = "";
-    spinner.classList.remove("hidden");
+    //spinner.classList.remove("hidden");
 
     try {
         
         //origin country
-        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         if (!response.ok) {
             throw new Error("Country not found");
         }
         const data = await response.json();
         const country = data[0];
-
-        // Update country info
-        // countryInfo.innerHTML = `
-        //     <h2>${country.name.common}</h2>
-        //     <p><strong>Capital:</strong> ${country.capital ? country.capital[0] : "N/A"}</p>
-        //     <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
-        //     <p><strong>Region:</strong> ${country.region}</p>
-        //     <img src="${country.flags.svg}" alt="${country.name.common} flag" width="150">
-        // `;
 
         let countryCapital = "N/A" ; //if no capital
         if (country.capital && country.capital.length>0){
@@ -46,50 +37,37 @@ async function searchCountry(countryName) {
 
         countryInfo.innerHTML = 
             "<h2>" + country.name.common + "</h2>" +
+            "<p><img src='" + country.flags.svg + "' alt='" + country.name.common + " flag' width='150'></p>" +
             "<p><strong>Capital:</strong> " + countryCapital + "</p>" +
             "<p><strong>Population:</strong> " + countryPopulation.toLocaleString() + "</p>" +
-            "<p><strong>Region:</strong> " + countryRegion + "</p>" +
-            "<img src='" + country.flags.svg + "' alt='" + country.name.common + " flag' width='150'>";
+            "<p><strong>Region:</strong> " + countryRegion + "</p>";
+            
 
         // Fetch bordering countries
-        // if (country.borders && country.borders.length > 0) {
-        //     for (let code of country.borders) {
-        //         const borderResponse = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
-        //         const borderData = await borderResponse.json();
-        //         const borderCountry = borderData[0];
-
-        //         bordersDiv.innerHTML += `
-        //             <div class="country-card">
-        //                 <p>${borderCountry.name.common}</p>
-        //                 <img src="${borderCountry.flags.svg}" alt="${borderCountry.name.common} flag" width="100">
-        //             </div>
-        //         `;
-        //     }
-        // } else {
-        //     bordersDiv.innerHTML = "<p>No bordering countries.</p>";
-        // }
 
         if (country.borders && country.borders.length > 0){
-            for (let i = 0; i <country.borders.lemngth; i++){
+            let bordersHTML = "<ul";
+            for (let i = 0; i <country.borders.length; i++){
                 const code = country.borders[i];
-
-                const borderResponse = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+                const borderResponse = await fetch("https://restcountries.com/v3.1/alpha/" + code);
                 const borderData = await borderResponse.json();
                 const borderCountry = borderData[0];
 
-                bordersDiv.innerHTML +=
-                    "<div class='country-card'>" +
+                bordersHTML +=
+                    "<li>" +
                         "<p>" + borderCountry.name.common + "</p>" +
                         "<img src='" + borderCountry.flags.svg + "' alt='" + borderCountry.name.common + " flag' width='100'>" +
-                    "</div>";
+                    "</li>";
             }
+            bordersHTML += "</ul>";
+            bordersDiv.innerHTML = bordersHTML;
         }
         else{
             bordersDiv.innerHTML = "<p>No bordering countries.</p>"
         }
 
     } catch (error) {
-        errorDiv.textContent = "⚠ Could not find that country. Please try again.";
+        errorDiv.textContent = " Could not find that country. Please try again.";
         errorDiv.classList.remove("hidden");
     } finally {
         spinner.classList.add("hidden");
